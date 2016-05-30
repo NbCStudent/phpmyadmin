789c2ebf6b5f9346e5533b1e00aae34561dd143a
Theo Tison <tison.theo@gmail.com>
2016-05-30 17:00:08 +0200

    connection BDD

---
+++ templates_c/1dc4f5a59a61af276cb727ec0da60c297bc9b4a3_0.file.home.tpl.php
@@ -0,0 +1,245 @@
+<?php
+/* Smarty version 3.1.29, created on 2016-05-30 16:47:48
+  from "/var/www/site_perso/my_phpmyadmin/phpmyadmin/Views/views/templates/home.tpl" */
+
+if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
+  'has_nocache_code' => false,
+  'version' => '3.1.29',
+  'unifunc' => 'content_574c5294857134_13701330',
+  'file_dependency' => 
+  array (
+    '1dc4f5a59a61af276cb727ec0da60c297bc9b4a3' => 
+    array (
+      0 => '/var/www/site_perso/my_phpmyadmin/phpmyadmin/Views/views/templates/home.tpl',
+      1 => 1464619659,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_574c5294857134_13701330 ($_smarty_tpl) {
+?>
+<div class="container interface">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="page-header">
+                        <img class="logoPhpmyadmin" src="Asset/img/logoPhpmyadmin.png">
+                    </div>
+                    <div class="col-md-3">
+                        <div class="jumbotron">
+                            <h3 class="titleJumbotron">Accueil</h3>
+                            <p class="titleJumbotron">
+                                <i class="fa fa-home" aria-hidden="true"></i>
+                                <i class="fa fa-question" aria-hidden="true"></i>
+                                <i class="fa fa-book" aria-hidden="true"></i>
+                                <i class="fa fa-repeat" aria-hidden="true"></i>
+                            </p>
+                            <p>
+                                Nouvelle base de données<br>
+                                etna_test<br>
+                                information<br>
+                                mysql<br>
+                                open_classroom<br>
+                                performance<br>
+                                SCC<br>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-9">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <div class="col-md-12">
+                                    <div class="panel panel-default">
+                                        <div class="panel-body">
+                                            <div class="tableView">
+                                                <table>
+                                                    <thead>
+                                                    <tr>
+                                                        <th></th>
+                                                        <th>#</th>
+                                                        <th>Nom</th>
+                                                        <th>Type</th>
+                                                        <th>Interclassement</th>
+                                                        <th>Attributs</th>
+                                                        <th>Null</th>
+                                                        <th>Défaut</th>
+                                                        <th>Extra</th>
+                                                        <th>Action</th>
+                                                    </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                    <tr>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>February</td>
+                                                        <td>$80</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>February</td>
+                                                        <td>$80</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>February</td>
+                                                        <td>$80</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                    </tr>
+                                                    </tbody>
+                                                    <tfoot>
+                                                    <tr>
+                                                        <td>Sum</td>
+                                                        <td>$180</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                        <td>January</td>
+                                                        <td>$100</td>
+                                                    </tr>
+                                                    </tfoot>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="panel panel-default">
+                                        <div class="panel-body">
+                                            <div class="tableView2">
+                                                <table class="table1">
+                                                    <thead class="thead1">
+                                                    <tr class="tr1">
+                                                        <th class="th1"></th>
+                                                        <th class="th1">#</th>
+                                                        <th class="th1">Nom</th>
+                                                        <th class="th1">Type</th>
+                                                    </tr>
+                                                    </thead>
+                                                    <tbody class="tbody1">
+                                                    <tr class="tr1">
+                                                        <td class="td1">January</td>
+                                                        <td class="td1">$100</td>
+                                                        <td class="td1">January</td>
+                                                        <td class="td1">$100</td>
+                                                    </tr>
+                                                    <tr class="tr1">
+                                                        <td class="td1">February</td>
+                                                        <td class="td1">$80</td>
+                                                        <td class="td1">January</td>
+                                                        <td class="td1">$100</td>
+                                                    </tr>
+                                                    <tr class="tr1">
+                                                        <td class="td1">January</td>
+                                                        <td class="td1">$100</td>
+                                                        <td class="td1">January</td>
+                                                        <td class="td1">$100</td>
+                                                    </tr>
+                                                    <tr class="tr1">
+                                                        <td class="td1">February</td>
+                                                        <td class="td1">$80</td>
+                                                        <td class="td1">January</td>
+                                                        <td class="td1">$100</td>
+                                                    </tr>
+                                                    <tr class="tr1">
+                                                        <td class="td1">January</td>
+                                                        <td class="td1">$100</td>
+                                                        <td class="td1">January</td>
+                                                        <td class="td1">$100</td>
+                                                    </tr>
+                                                    <tr class="tr1">
+                                                        <td class="td1">January</td>
+                                                        <td class="td1">$100</td>
+                                                        <td class="td1">January</td>
+                                                        <td class="td1">$100</td>
+                                                    </tr>
+                                                    <tr class="tr1">
+                                                        <td class="td1">January</td>
+                                                        <td class="td1">$100</td>
+                                                        <td class="td1">January</td>
+                                                        <td class="td1">$100</td>
+                                                    </tr>
+                                                    </tbody>
+                                                    <tfoot class="tfoot1">
+                                                    <tr class="tr1">
+                                                        <td class="td1">Sum</td>
+                                                        <td class="td1">$180</td>
+                                                        <td class="td1">January</td>
+                                                        <td class="td1">$100</td>
+                                                    </tr>
+                                                    </tfoot>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div><?php }
+}
