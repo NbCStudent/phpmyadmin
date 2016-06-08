6c83a2b08eaa8878486bdfcc848c7ff2f4cb9118
Pierre <riviera_paradise@hotmail.fr>
2016-06-08 14:05:59 +0200

    page 404

--- Controlleur/userControlleur.php
+++ Controlleur/userControlleur.php
@@ -15,10 +15,7 @@ if(isset($action) && $action == "login")
   }
 }
 elseif (isset($action) && $action == "checklogin" && !empty($_POST)) {
-<<<<<<< HEAD
-=======
 
->>>>>>> d689aff1f3b2aa74e8a1d6b7225ee216db5c8dd8
   $db = dbConnect("mysql");
   $Login = checklogin($db,$_POST['_username'],$_POST['_password']);
   if($Login == true)
@@ -35,10 +32,7 @@ elseif (isset($action) && $action == "checklogin" && !empty($_POST)) {
 elseif (isset($action) && $action == "logout") {
   logout();
   $template = "login";
-<<<<<<< HEAD
-=======
 }
 else {
   $template = "404";
->>>>>>> d689aff1f3b2aa74e8a1d6b7225ee216db5c8dd8
 }
--- templates_c/d4ef5b8459706ef752e8b9e716a2e89ed3814e2c_0.file.home.tpl.php
+++ templates_c/d4ef5b8459706ef752e8b9e716a2e89ed3814e2c_0.file.home.tpl.php
@@ -1,17 +1,17 @@
 <?php
-/* Smarty version 3.1.29, created on 2016-06-08 13:45:05
+/* Smarty version 3.1.29, created on 2016-06-08 14:02:32
   from "/Applications/MAMP/htdocs/TWEB-phpmyadmin/phpmyadmin/Views/views/templates/home.tpl" */
 
 if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
   'has_nocache_code' => false,
   'version' => '3.1.29',
-  'unifunc' => 'content_57580541919054_34813485',
+  'unifunc' => 'content_575809585414f3_98008410',
   'file_dependency' => 
   array (
     'd4ef5b8459706ef752e8b9e716a2e89ed3814e2c' => 
     array (
       0 => '/Applications/MAMP/htdocs/TWEB-phpmyadmin/phpmyadmin/Views/views/templates/home.tpl',
-      1 => 1465386298,
+      1 => 1465387350,
       2 => 'file',
     ),
   ),
@@ -19,7 +19,7 @@ if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl,
   array (
   ),
 ),false)) {
-function content_57580541919054_34813485 ($_smarty_tpl) {
+function content_575809585414f3_98008410 ($_smarty_tpl) {
 $_smarty_tpl->ext->_inheritance->init($_smarty_tpl, false);
 ?>
 <div class="container interface">
@@ -73,7 +73,7 @@ $_smarty_tpl->tpl_vars['dbname'] = $__foreach_dbname_0_saved_item;
                                 <div class="tableView">
                                     <?php 
 $_smarty_tpl->ext->_inheritance->processBlock($_smarty_tpl, 0, "tableBody", array (
-  0 => 'block_213202046657580541913b41_18254647',
+  0 => 'block_5364661135758095853ce69_05260771',
   1 => false,
   3 => 0,
   2 => 0,
@@ -91,7 +91,7 @@ $_smarty_tpl->ext->_inheritance->processBlock($_smarty_tpl, 0, "tableBody", arra
 </div>
 <?php }
 /* {block 'tableBody'}  file:Views/views/templates/home.tpl */
-function block_213202046657580541913b41_18254647($_smarty_tpl, $_blockParentStack) {
+function block_5364661135758095853ce69_05260771($_smarty_tpl, $_blockParentStack) {
 ?>
 
                                         <h1 class="informationMessage">WELCOME <?php echo $_smarty_tpl->tpl_vars['userName']->value;?>
